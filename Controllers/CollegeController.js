6545928c35fafeebb96d43258ad8c67b4bcfd0d8
anil babu <anilbabu3245@gmail.com>
2023-10-20 16:06:56 +0530

    working phone and email verification

--- Controllers/CollegeController.js
+++ Controllers/CollegeController.js
@@ -22,10 +22,11 @@ const Register = async (req, res) => {
     name,
     userType,
     email,
-    institutename,
+    Clientname,
     phoneno1,
     phoneno2,
     typeoforganization,
+    institutename,
     address,
     city,
     state,
@@ -33,25 +34,18 @@ const Register = async (req, res) => {
     password,
   } = req.body;
 
-  // if (!req.files.profileurl || !req.files.logourl) {
-  //   return respHandler.error(res, {
-  //     status: false,
-  //     msg: "Error in file uploading",
-  //   });
-  // }
-
   const genSalt = 10;
   const hash = await bcrypt.hash(password, genSalt);
+
   let clients = await getClientCount();
   let clientID = `CNO-${clients + 900}`;
-
   if (
     name != "" ||
     email != "" ||
     password != "" ||
-    institutename != "" ||
+    Clientname != "" ||
     phoneno1 != "" ||
-    phoneno2 != "" ||
+    institutename != "" ||
     typeoforganization != "" ||
     address != "" ||
     city != "" ||
@@ -60,54 +54,72 @@ const Register = async (req, res) => {
     userType != ""
   ) {
     try {
-      let user = await Client.findOne({ where: { email: email } });
+      let user = await Client.findOne({
+        where: { phoneno1: phoneno1, email: email },
+      });
       if (user != null) {
-        // removefile(`public/upload/${req.files.logourl[0].filename}`);
-        // removefile(`public/upload/${req.files.profileurl[0].filename}`);
-        return respHandler.error(res, {
-          status: false,
-          msg: "Email or Mobile Number already exist",
-        });
-      }
-      let newUser = {
-        name: name,
-        email: email,
-        institutename: institutename,
-        ClientCode: clientID,
-        phoneno1: phoneno1,
-        phoneno2: phoneno2,
-        typeoforganization: typeoforganization,
-        address: address,
-        city: city,
-        state: state,
-        pincode: pincode,
-        password: hash,
-        userType: userType,
-        // logourl: `images/${req.files.logourl[0].filename}`,
-        // profileurl: `images/${req.files.profileurl[0].filename}`,
-      };
-
-      let createdUser = await Client.create(newUser);
-      let clientdata = await Credentials.create(newUser);
-      var token = jwt.sign(
-        {
-          id: createdUser.id,
-          userType: createdUser.userType,
-        },
-        SECRET
-      );
-      if (token) {
-        return respHandler.success(res, {
-          status: true,
-          data: [{ User: createdUser, CollegeDetails: clientdata }],
-          msg: "College Account Created Successfully!!",
-        });
+        let status = await Client.update(
+          {
+            name: name,
+            email: email,
+            Clientname: Clientname,
+            phoneno1: phoneno1,
+            institutename: institutename,
+            ClientCode: clientID,
+            typeoforganization: typeoforganization,
+            address: address,
+            city: city,
+            state: state,
+            pincode: pincode,
+            password: hash,
+            userType: userType,
+            emailOtpStatus: true,
+            phoneOtpStatus: true,
+          },
+          {
+            where: {
+              phoneno1: phoneno1,
+              email: email,
+            },
+          }
+        );
+        if (status) {
+          let clientdata = await Credentials.create({
+            name: name,
+            email: email,
+            Clientname: Clientname,
+            phoneno1: phoneno1,
+            institutename: institutename,
+            ClientCode: clientID,
+            typeoforganization: typeoforganization,
+            address: address,
+            city: city,
+            state: state,
+            pincode: pincode,
+            password: hash,
+            userType: userType,
+          });
+          var token = jwt.sign(
+            {
+              id: user?.id,
+              userType: user?.userType,
+            },
+            SECRET
+          );
+          if (token) {
+            return respHandler.success(res, {
+              status: true,
+              msg: "Coaching Account Created Successfully!!",
+              data: [{ User: user, CollegeDetails: clientdata }],
+            });
+          }
+        }
       }
     } catch (err) {
       return respHandler.error(res, {
         status: false,
         msg: "Something Went Wrong!!",
-        error: [err.message],
+        error: [err.message]
       });
     }
   } else {
